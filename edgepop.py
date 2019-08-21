4b7825b1b2a8e26844a34d236865a33e69b79b7a
WilliamNHarvey <williamnharvey@gmail.com>
2019-08-21 10:24:03 -0700

    yeah yeah yeah yeah

--- edgepop.py
+++ edgepop.py
@@ -1,10 +1,24 @@
 from PIL import Image, ImageDraw, ImageFilter
 from math import sqrt
+import argparse
+import ntpath
 
-SPECIAL_SAUCE = 3
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", type=str, help="input image name", required=True)
+parser.add_argument("-r", "--red", type=float, help="percent red", default=1.0)
+parser.add_argument("-g", "--green", type=float, help="percent green", default=1.0)
+parser.add_argument("-b", "--blue", type=float, help="percent blue", default=1.0)
+parser.add_argument("-s", "--radius", type=int, help="line thickness radius", default=3)
+args = parser.parse_args()
+
+SPECIAL_SAUCE = args.radius
+FILENAME = ntpath.splitext(ntpath.basename(args.input))[0]
+RED = args.red
+GREEN = args.green
+BLUE = args.blue
 
 # Load image:
-input_image = Image.open("inputs/x.jpg")
+input_image = Image.open(args.input)
 smoothed_input = input_image.filter(ImageFilter.GaussianBlur(radius=SPECIAL_SAUCE))
 input_pixels = smoothed_input.load()
 
@@ -26,22 +40,28 @@ output_image = Image.new("RGBA", smoothed_input.size)
 
 draw = ImageDraw.Draw(output_image)
 
+full_size = (smoothed_input.width - 1) * (smoothed_input.height - 1)
+
 # Compute convolution between intensity and kernels
 for x in range(1, smoothed_input.width - 1):
-    for y in range(1, smoothed_input.height - 1):
-        magx, magy = 0, 0
-        for a in range(3):
-            for b in range(3):
-                xn = x + a - 1
-                yn = y + b - 1
-                magx += intensity[xn][yn] * SPECIAL_SAUCE * kernelx[a][b]
-                magy += intensity[xn][yn] * SPECIAL_SAUCE * kernely[a][b]
+  for y in range(1, smoothed_input.height - 1):
+    percent = round(((x * smoothed_input.height - 2) + y) / full_size * 100, 2)
+    print("\r{:.2f}% complete".format(percent), end = '')
+    magx, magy = 0, 0
+    for a in range(3):
+        for b in range(3):
+            xn = x + a - 1
+            yn = y + b - 1
+            magx += intensity[xn][yn] * kernelx[a][b] * SPECIAL_SAUCE
+            magy += intensity[xn][yn] * kernely[a][b] * SPECIAL_SAUCE
 
-        # Draw in black and white the magnitude
-        color = int(sqrt(magx**2 + magy**2))
-        draw.point((x, y), (color, color, 0, color))
+    # Cast to magnitude
+    color = int(sqrt(magx**2 + magy**2))
+    draw.point((x, y), (int(RED * color), int(GREEN * color), int(BLUE * color), color))
 
-output_image.save("edges/xedge.png")
+print("")
 
-input_image.paste(output_image, (0, 0), output_image);
-input_image.save("results/x.png");
+output_image.save("edges/{}.png".format(FILENAME))
+
+input_image.paste(output_image, (0, 0), output_image)
+input_image.save("results/{}.png".format(FILENAME))
